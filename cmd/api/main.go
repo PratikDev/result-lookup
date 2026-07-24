81da9b1d0093dcf3361f9df7858343a994029e28
Pratik Dev <pratikdevofficial1@gmail.com>
2026-07-24 23:53:01 +0600

    graceful shutdown for the http server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/pratikdev/result-lookup/internal/handlers"
@@ -17,7 +22,10 @@ func main() {
 	validate := validator.New()
 	mux := http.NewServeMux()
 
-	defer rdb.Close() // close connection when app exists
+	defer func() {
+		logger.Info("Closing downstream infra connections...")
+		rdb.Close()
+	}()
 
 	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
 		handlers.GetHealth(w, r)
@@ -26,5 +34,43 @@ func main() {
 		handlers.GetResult(w, r, rdb, logger, validate)
 	})
 
-	http.ListenAndServe(":" + os.Getenv("PORT"), mux)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// server configure
+	server := &http.Server{
+		Addr: ":" + port,
+		Handler: mux,
+		ReadTimeout: 15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+	}
+
+	// offload server-listening to a separate goroutine
+	go func () {
+		logger.Info("HTTP server spinning up", slog.String("port", port))
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Error("Critical server execution fault", slog.Any("error", err))
+			os.Exit(1)
+		}
+	}()
+
+	// register signal traps for standard os termination
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	// block main thread until an active termination signal arrives
+	<-ctx.Done()
+	logger.Info("Shutdown signal captured. Halting inbound traffic ingestion...")
+
+	// create a 10sec deadline context to complete in-flight requests
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		logger.Error("Server forced to shutdown with active connections dangling", slog.Any("error", err))
+	} else {
+		logger.Info("All in-flight requests completed successfully")
+	}
 }
